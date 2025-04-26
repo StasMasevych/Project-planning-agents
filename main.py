667f617b8a0ae30629515f8cd8a383e41584df4b
Stas Masevych <109438310+StasMasevych@users.noreply.github.com>
2025-04-26 17:49:40 +0200

    add initial setup

---
+++ main.py
@@ -0,0 +1,12 @@
+# Warning control
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load environment variables
+from helper import load_env
+load_env()
+
+import os
+import yaml
+from crewai import Agent, Task, Crew
+
